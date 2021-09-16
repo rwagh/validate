3a867c9fe2a5460f2518a5964692dbe674ec2fc7
Ravindra Wagh <rwagh@customtravelsolutions.com>
2021-09-16 17:59:53 +0530

    Updated

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 import date from "./date";
 module.export = {
+  capitalize: (value) => {
+    let wordArray = value.split(" ");
+    let words = [];
+    wordArray.forEach((word) => {
+      const lower = word.toLowerCase();
+      words.push(word.charAt(0).toUpperCase() + lower.slice(1));
+    });
+    return words.join(" ");
+  },
   isDate: (value) => {
     let pattern = /^\d{4}-\d{2}-\d{2}$/;
     return pattern.test(value);
@@ -36,7 +45,21 @@ module.export = {
     return value === undefined;
   },
   isNullOrEmpty: (value) => {
-    return value === null || value.trim() === "";
+    if (Array.isArray(value)) {
+      return value.length > 0;
+    } else if (typeof value === "object") {
+      return Object.keys(value).length > 0;
+    } else {
+      return value === null || value.trim() === "";
+    }
+  },
+  isNumber(value) {
+    return Number.isNaN(value);
+  },
+  isURL(string) {
+    var regex =
+      /(http|https):\/\/(\w+:{0,1}\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-\/]))?/;
+    return regex.test(string) ? true : false;
   },
   isPhone: (value) => {
     let pattern = /^[+]?\d+$/;
@@ -46,5 +69,13 @@ module.export = {
     const regx =
       /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return regx.test(String(email).toLowerCase());
+  },
+  isPassword(password) {
+    let pattern = /^(?=.\d)(?=.[!@#$%^&])(?=.[a-z])(?=.*[A-Z]).{7,}$/;
+    return pattern.test(password);
+  },
+  hasSpecialCharacters(value) {
+    let pattern = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
+    return pattern.test(value);
   },
 };
